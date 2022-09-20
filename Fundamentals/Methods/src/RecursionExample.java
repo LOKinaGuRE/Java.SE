fcdedea8606e437d82aedccdd2b7f1faf47a880c
LOKESHKUMAR <94413037+LOKinaGuRE@users.noreply.github.com>
2022-09-21 02:17:58 +0530

    Simple recursion code added

--- Fundamentals/Methods/src/RecursionExample.java
+++ Fundamentals/Methods/src/RecursionExample.java
@@ -1,6 +1,22 @@
 public class RecursionExample {
+    static int count = 5;
 
     public static void main(String[] args) {
+
+        display();
+
+    }
+
+    static void display() {
+        count--;
+        if (count > 0) {
+            System.out.println(count + "  Hello");
+            display();
+        }
+
+        // Hear display() function call itself untill count becomes less than or equal
+        // to zero
+        // when display() method called , than count value decread by 1 value
 
     }
 
